44c3128bbb2a434825f5799bf954a89cb31fcf81
maureenv <maureenvogel@outlook.com>
2018-01-06 14:53:34 -0500

    Add background and header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+      header
+      <div className="moon-rotation">
+        <div className="moon"></div>
+      </div>
+      </div>
+    )
+  }
+}
+
+
+export default Header
